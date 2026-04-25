5274ec30dafad0d4a1dd3f30a14095c3bcd63159
richmulumba131-droid <richmulumba131@gmail.com>
2026-04-25 09:33:10 +0200

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set DIRNAME=%~dp0
-set APP_BASE_NAME=%~n0
-set CLASSPATH=%DIRNAME%\gradle\wrapper\gradle-wrapper.jar
-
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
